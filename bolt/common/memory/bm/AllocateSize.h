012c37f760bd2320336a532a047dde4b8f783484
wangxinshuo.db <wangxinshuo.db@bytedance.com>
2026-06-02 17:19:37 +0800

    Derive memory tag count from enum

--- bolt/common/memory/bm/AllocateSize.h
+++ bolt/common/memory/bm/AllocateSize.h
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "bolt/common/base/Exceptions.h"
+
 namespace bytedance::bolt::memory::bm {
 
 enum class AllocateSize : uint8_t {
@@ -17,7 +19,22 @@ constexpr std::array<size_t, 3> kAllocateSizeBytes{
     1024 * 1024,
     4 * 1024 * 1024};
 
-size_t allocateSizeBytes(AllocateSize size);
-const char* toString(AllocateSize size);
+inline size_t allocateSizeBytes(AllocateSize size) {
+  const auto index = static_cast<size_t>(size);
+  BOLT_CHECK_LT(index, kAllocateSizeBytes.size());
+  return kAllocateSizeBytes[index];
+}
+
+inline const char* toString(AllocateSize size) {
+  switch (size) {
+    case AllocateSize::kSmall:
+      return "small";
+    case AllocateSize::kMedium:
+      return "medium";
+    case AllocateSize::kLarge:
+      return "large";
+  }
+  BOLT_FAIL("unknown BM allocate size {}", static_cast<int>(size));
+}
 
 } // namespace bytedance::bolt::memory::bm
