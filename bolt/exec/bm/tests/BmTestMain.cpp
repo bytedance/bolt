3b2ecd355055191bfc560fbdb903a5acfa2b78d9
wangxinshuo.db <wangxinshuo.db@bytedance.com>
2026-06-24 19:49:39 +0800

    exec/bm: initialize folly in bm tests

--- bolt/exec/bm/tests/BmTestMain.cpp
+++ bolt/exec/bm/tests/BmTestMain.cpp
@@ -1,9 +1,11 @@
 #include "bolt/common/memory/Memory.h"
 
+#include <folly/init/Init.h>
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
   bytedance::bolt::memory::MemoryManager::testingSetInstance({});
   testing::InitGoogleTest(&argc, argv);
+  folly::init(&argc, &argv, false);
   return RUN_ALL_TESTS();
 }
