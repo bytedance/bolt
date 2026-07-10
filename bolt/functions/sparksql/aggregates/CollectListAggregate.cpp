a633f4431b2bbe13b304ae7be0a419489c5ac53d
taiyang-li <taiyangli@apache.org>
2026-07-10 11:26:36 +0800

    Optimize collect_list intermediate path

--- bolt/functions/sparksql/aggregates/CollectListAggregate.cpp
+++ bolt/functions/sparksql/aggregates/CollectListAggregate.cpp
@@ -30,90 +30,220 @@
 
 #include "bolt/functions/sparksql/aggregates/CollectListAggregate.h"
 
-#include "bolt/exec/SimpleAggregateAdapter.h"
+#include <numeric>
+
+#include "bolt/exec/Aggregate.h"
 #include "bolt/functions/lib/aggregates/ValueList.h"
 using namespace bytedance::bolt::aggregate;
 using namespace bytedance::bolt::exec;
 namespace bytedance::bolt::functions::aggregate::sparksql {
 namespace {
-class CollectListAggregate {
+struct CollectListAccumulator {
+  ValueList elements;
+};
+
+class CollectListAggregate : public exec::Aggregate {
  public:
-  using InputType = Row<Generic<T1>>;
+  explicit CollectListAggregate(TypePtr resultType)
+      : exec::Aggregate(std::move(resultType)) {}
 
-  using IntermediateType = Array<Generic<T1>>;
+  int32_t accumulatorFixedWidthSize() const final {
+    return sizeof(CollectListAccumulator);
+  }
 
-  using OutputType = Array<Generic<T1>>;
-
-  /// In Spark, when all inputs are null, the output is an empty array instead
-  /// of null. Therefore, in the writeIntermediateResult and writeFinalResult,
-  /// we still need to output the empty element_ when the group is null. This
-  /// behavior can only be achieved when the default-null behavior is disabled.
-  static constexpr bool default_null_behavior_ = false;
-
-  static bool toIntermediate(
-      exec::out_type<Array<Generic<T1>>>& out,
-      exec::optional_arg_type<Generic<T1>> in) {
-    if (in.has_value()) {
-      out.add_item().copy_from(in.value());
-      return true;
-    }
+  bool isFixedSize() const final {
     return false;
   }
 
-  struct AccumulatorType {
-    ValueList elements_;
+  bool supportsToIntermediate() const final {
+    return true;
+  }
 
-    explicit AccumulatorType(
-        HashStringAllocator* /*allocator*/,
-        CollectListAggregate* /*fn*/)
-        : elements_{} {}
+  FLATTEN void toIntermediate(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      VectorPtr& result) const final {
+    const auto& elements = args[0];
+    const auto numRows = rows.size();
+    auto* pool = allocator_->pool();
 
-    static constexpr bool is_fixed_size_ = false;
+    BufferPtr nulls = allocateNulls(numRows, pool);
+    auto* rawNulls = nulls->asMutable<uint64_t>();
+    memcpy(rawNulls, rows.asRange().bits(), bits::nbytes(numRows));
 
-    bool addInput(
-        HashStringAllocator* allocator,
-        exec::optional_arg_type<Generic<T1>> data) {
-      if (data.has_value()) {
-        elements_.appendValue(data, allocator);
-        return true;
+    auto loadedElements = BaseVector::loadedVectorShared(elements);
+    if (loadedElements->mayHaveNulls()) {
+      rows.applyToSelected([&](vector_size_t row) {
+        if (loadedElements->isNullAt(row)) {
+          bits::setNull(rawNulls, row);
+        }
+      });
+    }
+
+    BufferPtr offsets = allocateOffsets(numRows, pool);
+    auto* rawOffsets = offsets->asMutable<vector_size_t>();
+    std::iota(rawOffsets, rawOffsets + numRows, 0);
+
+    BufferPtr sizes = allocateSizes(numRows, pool);
+    auto* rawSizes = sizes->asMutable<vector_size_t>();
+    std::fill(rawSizes, rawSizes + numRows, 1);
+
+    result = std::make_shared<ArrayVector>(
+        pool,
+        ARRAY(elements->type()),
+        nulls,
+        numRows,
+        offsets,
+        sizes,
+        loadedElements);
+  }
+
+  void initializeNewGroups(
+      char** groups,
+      folly::Range<const vector_size_t*> indices) final {
+    for (auto index : indices) {
+      new (groups[index] + offset_) CollectListAccumulator();
+    }
+  }
+
+  FLATTEN void
+  extractValues(char** groups, int32_t numGroups, VectorPtr* result) final {
+    auto vector = (*result)->as<ArrayVector>();
+    BOLT_CHECK(vector);
+    vector->resize(numGroups);
+
+    auto elements = vector->elements();
+    elements->resize(countElements(groups, numGroups));
+
+    uint64_t* rawNulls = getRawNulls(vector);
+    vector_size_t offset = 0;
+    for (int32_t i = 0; i < numGroups; ++i) {
+      auto& values = value(groups[i])->elements;
+      const auto arraySize = values.size();
+      clearNull(rawNulls, i);
+      vector->setOffsetAndSize(i, offset, arraySize);
+
+      if (arraySize) {
+        ValueListReader reader(values);
+        for (auto index = 0; index < arraySize; ++index) {
+          reader.next(*elements, offset + index);
+        }
+        offset += arraySize;
       }
-      return false;
     }
+  }
 
-    bool combine(
-        HashStringAllocator* allocator,
-        exec::optional_arg_type<IntermediateType> other) {
-      if (!other.has_value()) {
-        return false;
+  FLATTEN void extractAccumulators(
+      char** groups,
+      int32_t numGroups,
+      VectorPtr* result) final {
+    extractValues(groups, numGroups, result);
+  }
+
+  FLATTEN void addRawInput(
+      char** groups,
+      const SelectivityVector& rows,
+      const std::vector<VectorPtr>& args,
+      bool /*mayPushdown*/) final {
+    decodedElements_.decode(*args[0], rows);
+    rows.applyToSelected([&](vector_size_t row) {
+      if (decodedElements_.isNullAt(row)) {
+        return;
       }
-      for (auto element : other.value()) {
-        elements_.appendValue(element, allocator);
+
+      auto tracker = trackRowSize(groups[row]);
+      value(groups[row])->elements.appendValue(
+          decodedElements_, row, allocator_);
+    });
+  }
+
+  FLATTEN void addIntermediateResults(
+      char** groups,
+      const SelectivityVector& rows,
+      const std::vector<VectorPtr>& args,
+      bool /*mayPushdown*/) final {
+    decodedIntermediate_.decode(*args[0], rows);
+
+    auto arrayVector = decodedIntermediate_.base()->as<ArrayVector>();
+    auto elements = arrayVector->elements();
+    rows.applyToSelected([&](vector_size_t row) {
+      if (decodedIntermediate_.isNullAt(row)) {
+        return;
       }
-      return true;
-    }
 
-    bool writeIntermediateResult(
-        bool /*nonNullGroup*/,
-        exec::out_type<IntermediateType>& out) {
-      // If the group's accumulator is null, the corresponding intermediate
-      // result is an empty array.
-      copyValueListToArrayWriter(out, elements_);
-      return true;
-    }
+      const auto decodedRow = decodedIntermediate_.index(row);
+      auto tracker = trackRowSize(groups[row]);
+      value(groups[row])->elements.appendRange(
+          elements,
+          arrayVector->offsetAt(decodedRow),
+          arrayVector->sizeAt(decodedRow),
+          allocator_);
+    });
+  }
 
-    bool writeFinalResult(
-        bool /*nonNullGroup*/,
-        exec::out_type<OutputType>& out) {
-      // If the group's accumulator is null, the corresponding result is an
-      // empty array.
-      copyValueListToArrayWriter(out, elements_);
-      return true;
-    }
+  void addSingleGroupRawInput(
+      char* group,
+      const SelectivityVector& rows,
+      const std::vector<VectorPtr>& args,
+      bool /*mayPushdown*/) final {
+    auto& values = value(group)->elements;
 
-    void destroy(HashStringAllocator* allocator) {
-      elements_.free(allocator);
+    decodedElements_.decode(*args[0], rows);
+    auto tracker = trackRowSize(group);
+    rows.applyToSelected([&](vector_size_t row) {
+      if (decodedElements_.isNullAt(row)) {
+        return;
+      }
+      values.appendValue(decodedElements_, row, allocator_);
+    });
+  }
+
+  void addSingleGroupIntermediateResults(
+      char* group,
+      const SelectivityVector& rows,
+      const std::vector<VectorPtr>& args,
+      bool /*mayPushdown*/) final {
+    decodedIntermediate_.decode(*args[0], rows);
+
+    auto arrayVector = decodedIntermediate_.base()->as<ArrayVector>();
+    auto elements = arrayVector->elements();
+    auto& values = value(group)->elements;
+    auto tracker = trackRowSize(group);
+    rows.applyToSelected([&](vector_size_t row) {
+      if (decodedIntermediate_.isNullAt(row)) {
+        return;
+      }
+
+      const auto decodedRow = decodedIntermediate_.index(row);
+      values.appendRange(
+          elements,
+          arrayVector->offsetAt(decodedRow),
+          arrayVector->sizeAt(decodedRow),
+          allocator_);
+    });
+  }
+
+  void destroy(folly::Range<char**> groups) final {
+    for (auto group : groups) {
+      value(group)->elements.free(allocator_);
     }
-  };
+  }
+
+ private:
+  CollectListAccumulator* value(char* group) const {
+    return reinterpret_cast<CollectListAccumulator*>(group + offset_);
+  }
+
+  vector_size_t countElements(char** groups, int32_t numGroups) const {
+    vector_size_t size = 0;
+    for (int32_t i = 0; i < numGroups; ++i) {
+      size += value(groups[i])->elements.size();
+    }
+    return size;
+  }
+
+  DecodedVector decodedElements_;
+  DecodedVector decodedIntermediate_;
 };
 
 AggregateRegistrationResult registerCollectList(
@@ -138,8 +268,7 @@ AggregateRegistrationResult registerCollectList(
           /*config*/) -> std::unique_ptr<exec::Aggregate> {
         BOLT_CHECK_EQ(
             argTypes.size(), 1, "{} takes at most one argument", name);
-        return std::make_unique<SimpleAggregateAdapter<CollectListAggregate>>(
-            step, argTypes, resultType);
+        return std::make_unique<CollectListAggregate>(resultType);
       },
       withCompanionFunctions,
       overwrite);
